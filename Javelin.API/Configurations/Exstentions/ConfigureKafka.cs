b291d118b3d28b9981a25672a01759fd3d32204d
KeanuKapank <103486515+KeanuKapank@users.noreply.github.com>
2026-06-16 20:04:50 +0200

    Chore : Added Kafka Producer and Configurations (#4)

---
+++ Javelin.API/Configurations/Exstentions/ConfigureKafka.cs
@@ -0,0 +1,14 @@
+﻿using Confluent.Kafka;
+using Javelin.API.Configurations.Models;
+using Javelin.API.Infrastructure;
+
+namespace Javelin.API.Configurations.Exstentions
+{
+    public static class ConfigureKafka
+    {
+        public static void ConfigureKafkaServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProducer, Producer>();
+        }
+    }
+}
--- Javelin.API/Program.cs
+++ Javelin.API/Program.cs
@@ -1,4 +1,7 @@
 
+using Javelin.API.Configurations.Exstentions;
+using Javelin.API.Configurations.Models;
+
 namespace Javelin.API
 {
     public class Program
@@ -12,6 +15,13 @@ namespace Javelin.API
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+
+
+            AppConfig appConfig = new();
+            builder.Configuration.GetSection("AppConfig").Bind(appConfig);
+            builder.Services.AddSingleton(appConfig);
+
+            builder.Services.ConfigureKafkaServices();
 
             var app = builder.Build();
 
